5e9de2a71a5033893789312efcaccda38c05e381
Vinicius Sales <vinicius.2010.s@gmail.com>
2021-07-22 12:43:20 -0300

    enhance: fixtures types

--- src/config/tests/fixtures/index.ts
+++ src/config/tests/fixtures/index.ts
@@ -5,16 +5,16 @@ export function unsafe <T> (value: unknown): T {
   return value as T
 }
 
-type Callback = (a: unknown) => unknown
-type MapAll = (fn: Callback) => (data: TE.TaskEither<unknown, unknown>) =>
-  TE.TaskEither<unknown, unknown>
+type Callback<E, T> = (a: E | T) => unknown
 
-export const mapAll: MapAll = (fn) => (data) => {
-  return pipe(
-    data,
-    TE.map(fn),
-    TE.mapLeft(fn),
-  )
+export function mapAll<E, T> (fn: Callback<E, T>) {
+  return function (data: TE.TaskEither<E, T>) {
+    return pipe(
+      data,
+      TE.map(fn),
+      TE.mapLeft(fn),
+    )
+  }
 }
 
 export function getErrorMessage (errors: unknown): string {
