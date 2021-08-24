d7b84ac446ac540cdf566b4f003c1ae03c40f329
Fernando Daciuk <f.daciuk@gmail.com>
2021-08-24 08:26:38 -0300

    Finish fastify implementation

--- src/ports/fastify/server.ts
+++ src/ports/fastify/server.ts
@@ -1,12 +1,17 @@
 import fastify, {
   RouteShorthandOptions,
   RawReplyDefaultExpression,
+  FastifyRequest,
+  FastifyReply,
+  HookHandlerDoneFunction,
 } from 'fastify'
 import { pipe } from 'fp-ts/function'
 import * as TE from 'fp-ts/TaskEither'
 import { env } from '@/helpers/env'
+import { Slug } from '@/core/types/slug'
 import { CreateUser, LoginUser } from '@/core/user/types'
 import { CreateArticle, AuthorId } from '@/core/article/types'
+import { CreateComment } from '@/core/comment/types'
 import { getError } from '@/ports/adapters/http/http'
 import * as user from '@/ports/adapters/http/modules/user'
 import * as article from '@/ports/adapters/http/modules/article'
@@ -32,7 +37,7 @@ app.post<ApiUsers>('/api/users', async (req, reply) => {
     req.body.user,
     user.registerUser,
     TE.map(result => reply.send(result)),
-    TE.mapLeft(error => reply.status(422).send(error)),
+    TE.mapLeft(error => reply.code(422).send(error)),
   )()
 })
 
@@ -47,9 +52,24 @@ app.post<UsersLogin>('/api/users/login', async (req, reply) => {
     req.body.user,
     user.login,
     TE.map(result => reply.send(result)),
-    TE.mapLeft(error => reply.status(422).send(error)),
+    TE.mapLeft(error => reply.code(422).send(error)),
   )()
 })
+
+const auth = async <T>(
+  req: FastifyRequest<T, http.Server, CustomRequest>,
+  reply: FastifyReply,
+  done: HookHandlerDoneFunction,
+) => {
+  try {
+    const token = req.headers.authorization?.replace('Bearer ', '') ?? ''
+    const payload = await verifyToken(token)
+    req.raw.auth = payload
+    done()
+  } catch {
+    reply.code(401).send(getError('Unauthorized'))
+  }
+}
 
 type ApiArticles = {
   Body: {
@@ -57,33 +77,23 @@ type ApiArticles = {
   }
 }
 
-type FastifyOptions = RouteShorthandOptions<
+type FastifyApiArticleOptions = RouteShorthandOptions<
   http.Server,
   CustomRequest,
   RawReplyDefaultExpression<http.Server>,
   ApiArticles
 >
 
-const articleOptions: FastifyOptions = {
-  preValidation: async (req, reply, done) => {
-    try {
-      const token = req.headers.authorization?.replace('Bearer ', '') ?? ''
-      const payload = await verifyToken(token)
-      req.raw.auth = payload
-      done()
-    } catch {
-      reply.status(401).send(getError('Unauthorized'))
-    }
-  },
+const articleApiOptions: FastifyApiArticleOptions = {
+  preValidation: (...args) => auth<ApiArticles>(...args),
 }
 
-app.post<ApiArticles>('/api/articles', articleOptions, async (req, reply) => {
+app.post<ApiArticles>('/api/articles', articleApiOptions, async (req, reply) => {
   const payload = req.raw.auth
   const idProp = 'id'
 
   const data = {
     ...req.body.article,
-    // TODO: Try to remove type assertion
     authorId: payload?.[idProp] as AuthorId,
   }
 
@@ -91,7 +101,46 @@ app.post<ApiArticles>('/api/articles', articleOptions, async (req, reply) => {
     data,
     article.registerArticle,
     TE.map(result => reply.send(result)),
-    TE.mapLeft(error => reply.status(422).send(error)),
+    TE.mapLeft(error => reply.code(422).send(error)),
+  )()
+})
+
+type ApiAddComment = {
+  Body: {
+    comment: CreateComment
+  }
+
+  Params: {
+    slug: Slug
+  }
+}
+
+type FastifyApiAddCommentOptions = RouteShorthandOptions<
+  http.Server,
+  CustomRequest,
+  RawReplyDefaultExpression<http.Server>,
+  ApiAddComment
+>
+
+const addCommentOptions: FastifyApiAddCommentOptions = {
+  preValidation: (...args) => auth<ApiAddComment>(...args),
+}
+
+app.post<ApiAddComment>('/api/articles/:slug/comments', addCommentOptions, async (req, reply) => {
+  const payload = req.raw.auth
+  const idProp = 'id'
+
+  const data = {
+    ...req.body.comment,
+    authorId: payload?.[idProp] as AuthorId,
+    articleSlug: req.params.slug,
+  }
+
+  return pipe(
+    data,
+    article.addCommentToAnArticle,
+    TE.map(result => reply.send(result)),
+    TE.mapLeft(error => reply.code(422).send(error)),
   )()
 })
 
