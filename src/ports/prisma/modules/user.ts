1a2512f3d7ae26b4f46c52dcd15856225e8b3676
Fernando Daciuk <f.daciuk@gmail.com>
2021-09-30 12:01:59 -0300

    Abstraction for login

--- src/ports/prisma/modules/user.ts
+++ src/ports/prisma/modules/user.ts
@@ -1,6 +1,10 @@
+import argon2 from 'argon2'
+import { User } from '@prisma/client'
 import { CreateUserInDB } from '@/ports/adapters/db/types'
+import { UpdateUser, LoginUser } from '@/core/user/types'
 import {
   ValidationError,
+  NotFoundError,
 } from '@/helpers/errors'
 import { prisma } from '../prisma'
 
@@ -19,4 +23,13 @@ export const createUserInDB: CreateUserInDB = async (data) => {
   } catch (e) {
     throw new ValidationError('User already registered')
   }
+}
+
+type Login = (data: LoginUser) => Promise<User | null>
+export const login: Login = (data) => {
+  return prisma.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  })
 }
