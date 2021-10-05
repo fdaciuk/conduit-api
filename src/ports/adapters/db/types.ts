988b27c822bde851dd89c23836f008072656ab9f
Fernando Daciuk <f.daciuk@gmail.com>
2021-10-05 08:06:58 -0300

    Isolate db common types and abstraction for getProfileFromDB

--- src/ports/adapters/db/types.ts
+++ src/ports/adapters/db/types.ts
@@ -2,6 +2,7 @@ import {
   UserOutput,
   CreateUserOutput,
   UpdateUserOutput,
+  LoginUser,
 } from '@/core/user/types'
 
 export type DBUser = Omit<UserOutput, 'token'> & {
@@ -15,11 +16,19 @@ type CreateUserData = CreateUserOutput & {
   password: string
 }
 
-export type CreateUserInDB = (data: CreateUserData) => Promise<DBUser>
+export type CreateUserInDB<T = DBUser> = (data: CreateUserData) => Promise<T>
+
+export type Login<T = DBUser> = (data: LoginUser) => Promise<T | null>
 
 type UpdateUserData = UpdateUserOutput & {
   password?: string
 }
 
-export type UpdateUserInDB = (id: string) =>
-  (data: UpdateUserData) => Promise<DBUser>
+export type UpdateUserInDB<T = DBUser> = (id: string) =>
+  (data: UpdateUserData) => Promise<T>
+
+export type GetCurrentUserFromDB<T = DBUser> = (id: string) =>
+  Promise<T | null>
+
+export type GetProfileFromDB<T = DBUser> = (username: string) =>
+  Promise<T | null>
--- src/ports/prisma/modules/user.ts
+++ src/ports/prisma/modules/user.ts
@@ -1,13 +1,18 @@
 import { User } from '@prisma/client'
-import { CreateUserInDB, UpdateUserInDB } from '@/ports/adapters/db/types'
-import { LoginUser } from '@/core/user/types'
+import {
+  CreateUserInDB,
+  UpdateUserInDB,
+  GetCurrentUserFromDB,
+  GetProfileFromDB,
+  Login,
+} from '@/ports/adapters/db/types'
 import {
   ValidationError,
   NotFoundError,
 } from '@/helpers/errors'
 import { prisma } from '../prisma'
 
-export const createUserInDB: CreateUserInDB = async (data) => {
+export const createUserInDB: CreateUserInDB<User> = async (data) => {
   try {
     const result = await prisma.user.create({
       data: {
@@ -17,15 +22,13 @@ export const createUserInDB: CreateUserInDB = async (data) => {
       },
     })
 
-    const { bio, image, ...resultWithoutBioAndImage } = result
-    return resultWithoutBioAndImage
+    return result
   } catch (e) {
     throw new ValidationError('User already registered')
   }
 }
 
-type Login = (data: LoginUser) => Promise<User | null>
-export const login: Login = (data) => {
+export const login: Login<User> = (data) => {
   return prisma.user.findUnique({
     where: {
       email: data.email,
@@ -33,7 +36,7 @@ export const login: Login = (data) => {
   })
 }
 
-export const updateUserInDB: UpdateUserInDB = (id) => async (data) => {
+export const updateUserInDB: UpdateUserInDB<User> = (id) => async (data) => {
   try {
     const user = await prisma.user.update({
       where: {
@@ -48,11 +51,7 @@ export const updateUserInDB: UpdateUserInDB = (id) => async (data) => {
       },
     })
 
-    return {
-      ...user,
-      bio: user.bio ?? undefined,
-      image: user.image ?? undefined,
-    }
+    return user
   } catch (e) {
     if (e.message.includes('constraint failed on the fields: (`email`)')) {
       throw new ValidationError('This email is already in use')
@@ -66,10 +65,14 @@ export const updateUserInDB: UpdateUserInDB = (id) => async (data) => {
   }
 }
 
-export const getCurrentUserFromDB = async (id: string) => {
+export const getCurrentUserFromDB: GetCurrentUserFromDB<User> = async (id) => {
   return prisma.user.findUnique({
-    where: {
-      id,
-    },
+    where: { id },
+  })
+}
+
+export const getProfileFromDB: GetProfileFromDB<User> = async (username) => {
+  return prisma.user.findUnique({
+    where: { username },
   })
 }
