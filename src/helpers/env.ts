c61bb33770a2b81ce427bf7d80c695fcf4d51664
Fernando Daciuk <f.daciuk@gmail.com>
2021-10-14 06:50:36 -0300

    Select port via env vars

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,18 +1,22 @@
+import * as t from 'io-ts'
 import { failure } from 'io-ts/PathReporter'
 import * as E from 'fp-ts/Either'
 import { pipe } from 'fp-ts/function'
-import { withMessage, NonEmptyString } from 'io-ts-types'
+import { withMessage, NonEmptyString, NumberFromString } from 'io-ts-types'
 import { ValidationError } from '@/helpers/errors'
 
-type Envs =
-  | 'PORT'
-  | 'JWT_SECRET'
+const envsCodecs = {
+  HTTP_PORT: createUnionCodec('HTTP_PORT', ['express', 'fastify']),
+  DB_PORT: createUnionCodec('DB_PORT', ['db-in-memory', 'prisma']),
+  JWT_PORT: createUnionCodec('JWT_PORT', ['jose']),
+  PORT: withMessage(NumberFromString, () => '"PORT" should be a number'),
+  JWT_SECRET: t.string,
+}
 
-export const env = (value: Envs) => {
-  const envCodec = withMessage(
-    NonEmptyString,
-    () => `You must set the env var ${value}`,
-  )
+type Envs = keyof typeof envsCodecs
+
+export const env = (value: Envs): string => {
+  const envCodec = createEnvCodec(value)
 
   return pipe(
     envCodec.decode(process.env[value]),
@@ -20,5 +24,29 @@ export const env = (value: Envs) => {
       (errors) => { throw new ValidationError(failure(errors).join(':::')) },
       (value) => value,
     ),
+  )
+}
+
+const createEnvCodec = (value: Envs) => {
+  return t.intersection([
+    withMessage(NonEmptyString, () => `You must set the env var ${value}`),
+    envsCodecs[value],
+  ])
+}
+
+function createUnionCodec (name: string, values: string[]) {
+  const valuesObj = values.reduce<Record<string, null>>((acc, value) => {
+    acc[value] = null
+    return acc
+  }, {})
+
+  const valuesMessage = values
+    .map(v => `"${v}"`)
+    .join(', ')
+    .replace(/, "(\w+)"$/, ' or "$1"')
+
+  return withMessage(
+    t.keyof(valuesObj),
+    () => `"${name}" only accepts ${valuesMessage}`,
   )
 }
--- src/ports/adapters/http/index.ts
+++ src/ports/adapters/http/index.ts
@@ -1,1 +1,8 @@
-export * from './server'
+import { env } from '@/helpers/env'
+
+const HTTP_PORT = env('HTTP_PORT')
+
+import(`@/ports/${HTTP_PORT}`).then((http) => {
+  console.log('HTTP_PORT:', HTTP_PORT)
+  http.start()
+})
