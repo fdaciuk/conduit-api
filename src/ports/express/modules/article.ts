6310b1839ea66f21169eeff441bd9a92afff8e53
Fernando Daciuk <f.daciuk@gmail.com>
2021-10-27 07:39:29 -0300

    Add implementation to get comments from an article

--- src/ports/express/modules/article.ts
+++ src/ports/express/modules/article.ts
@@ -95,6 +95,23 @@ articleRoutes.post('/api/articles/:slug/comments', auth, async (req: Request, re
   )()
 })
 
+articleRoutes.get('/api/articles/:slug/comments', tryAuth, (req: Request, res: Response) => {
+  const payload = getPayload(req.auth)
+  const propSlug = 'slug'
+
+  const data = {
+    slug: req.params[propSlug] ?? '',
+    userId: payload.id,
+  }
+
+  pipe(
+    data,
+    article.getCommentsFromAnArticle,
+    TE.map(result => res.json(result)),
+    TE.mapLeft(result => res.status(result.code).json(result.error)),
+  )()
+})
+
 articleRoutes.get('/api/tags', (_req, res) => {
   pipe(
     article.getTags(),
